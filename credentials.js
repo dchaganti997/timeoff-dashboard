261db7f90f953c5dee970fd4974bc671fc10b17f
dchaganti997 <dchaganti@binghamton.edu>
2025-11-15 03:50:41 -0500

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,25 @@
+// SIMPLE SODEXO MANAGER LOGIN CREDENTIALS
+// (Plain-text based, internal-use only)
+
+const MANAGER_CREDENTIALS = {
+  "c4_manager": {
+      password: "C4admin@2025",
+      location: "C4"
+  },
+  "app_manager": {
+      password: "AppAdmin@2025",
+      location: "Appalachian"
+  },
+  "ciw_manager": {
+      password: "CIWadmin@2025",
+      location: "CIW"
+  },
+  "hinman_manager": {
+      password: "Hinman2025",
+      location: "Hinman"
+  },
+  "einstein_manager": {
+      password: "Einstein2025",
+      location: "Einstein"
+  }
+};
