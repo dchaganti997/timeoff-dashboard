d8f8e5b8bc146771aae397df905d2a00ac1a143f
dchaganti997 <dchaganti@binghamton.edu>
2025-11-15 05:39:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/***** CONFIG *****/
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwS-A6B_OpqyxDwqG7swgJb-4tCZcaFEvNwYf5T16HptLm4LMaLD7G2zJM2EKPzgw/exec";
 const API_TOKEN  = "DCHAGANTI_TIMEOFF_9A83B7X2";
 
